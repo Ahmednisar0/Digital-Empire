3f1c835141e41e0f0a4cc39e254e21b034efa0c2
ahmedraza <aaza60232@gmail.com>
2025-02-26 07:19:31 +0500

    changes

--- components/aboutsec.tsx
+++ components/aboutsec.tsx
@@ -26,11 +26,10 @@ Our team helps businesses grow with smart strategies, eye-catching designs, and 
 We create strong brand identities, high-quality websites, and marketing campaigns that deliver real results.
 Whether you're starting fresh or looking to expand, we’re here to turn your ideas into success.
         </p>
-        <p className="text-gray-600 lg:hidden block mt-8 text-lg w-[300px] mx-auto lg:mx-0">
-        At Digital Empire, we bring creativity and technology together to build powerful digital solutions.
+        <p className="text-gray-600 lg:hidden text-center block mt-8 text-lg w-[370px] mx-auto lg:mx-0">
+        At Digital Empire, we bring<br/> creativity and technology together to<br/> build powerful digital solutions.
 Our team helps businesses grow with smart strategies, eye-catching designs, and smooth development.
 We create strong brand identities, high-quality websites, and marketing campaigns that deliver real results.
-Whether you're starting fresh or looking to expand, we’re here to turn your ideas into success.
         </p>
         <motion.a
           href="/"
